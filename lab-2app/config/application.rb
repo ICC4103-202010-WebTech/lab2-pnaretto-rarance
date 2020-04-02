0b23631609e2ebca2b760644e218dba6ffcbecd5
PedroNaretto <ppnaretto@miuandes.cl>
2020-04-02 17:29:09 -0500

    new app

--- lab-2app/config/application.rb
+++ lab-2app/config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Lab3app
+module Lab2app
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
--- lab-2app/config/environments/production.rb
+++ lab-2app/config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "lab_3app_production"
+  # config.active_job.queue_name_prefix = "lab_2app_production"
 
   config.action_mailer.perform_caching = false
 
