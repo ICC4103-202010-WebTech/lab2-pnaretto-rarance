d46dba3ead7ddb16c373bc6388c956dd36a06a4d
PedroNaretto <ppnaretto@miuandes.cl>
2020-04-03 00:51:15 -0300

    Ubuntu ctm! Ahora trabajamos en VB

--- lab-3app/config/application.rb
+++ lab-3app/config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Lab2app
+module Lab3app
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
